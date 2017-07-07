6523d995267d1abdcedb6bd173866a507a9f16ff
m.karpov <karpovprog@yandex.ru>
2017-07-07 22:21:33 +0400

    Add vacancy.list component and template to this component, write component logic

---
+++ local/components/my/vacancy.list/class.php
@@ -0,0 +1,46 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+
+class CVacancyList extends CBitrixComponent {
+
+    public function executeComponent()
+    {
+        global $arParams;
+        global $arResult;
+        CModule::IncludeModule("iblock");
+
+        $elementOnPage = 10;
+
+        $arSort = array(
+            "active_from" => "desc",
+            "name" => "asc",
+        );
+
+        $arFilter = array(
+            "IBLOCK_ID" => $this->arParams["IBLOCK_ID"],
+        );
+
+        $arNavParams = array(
+            "nPageSize" => $elementOnPage,
+        );
+
+        $arSelect = array("ID", "IBLOCK_ID", "NAME", "PROPERTY_PAYMENT", "PROPERTY_PAYMENT_UP_TO", "PROPERTY_SPEC", "PROPERTY_EMPLOYER");
+
+        $listOfElements = CIBlockElement::GetList($arSort, $arFilter, false, $arNavParams, $arSelect);
+        $listOfElements->SetUrlTemplates("/vacancy/#ELEMENT_ID#/", "", "/vacancy/");
+
+        $element = $listOfElements->GetNextElement();
+        while($element){
+
+            $item = $element->GetFields();
+            $item["PROPERTIES"] = $element->GetProperties();
+
+            $arResult["ITEMS"][] = $item;
+            $arResult["ELEMENTS"][] = $item["ID"];
+
+            $element = $listOfElements->GetNextElement();
+        }
+
+        $this->includeComponentTemplate();
+    }
+
+}
---
+++ local/components/my/vacancy.list/templates/.default/template.php
@@ -0,0 +1,10 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+global $arResult;
+?>
+
+<div class="container">
+    <div class="row">
+        <h1>List</h1>
+    </div>
+</div>
+
